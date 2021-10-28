0d7cf5cf5d0e5d73c50e8461744b522e3a4d59aa
Adi Andrea <dekz.itfz@gmail.com>
2021-10-29 07:25:22 +0800

    add cta to login ui

--- app/src/main/java/com/demajors/demajorsapp/feature/profile/ProfileFragment.kt
+++ app/src/main/java/com/demajors/demajorsapp/feature/profile/ProfileFragment.kt
@@ -1,11 +1,13 @@
 package com.demajors.demajorsapp.feature.profile
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.demajors.demajorsapp.base.BaseFragment
 import com.demajors.demajorsapp.databinding.FragmentProfileBinding
+import com.demajors.demajorsapp.feature.login.LoginActivity
 
 class ProfileFragment : BaseFragment<ProfileViewModel>() {
     override val viewModelClass: Class<ProfileViewModel> = ProfileViewModel::class.java
@@ -28,5 +30,13 @@ class ProfileFragment : BaseFragment<ProfileViewModel>() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        _binding?.let { ui ->
+            ui.actionLogout.setOnClickListener {
+                startActivity(
+                    Intent(activity, LoginActivity::class.java)
+                )
+            }
+        }
     }
 }
